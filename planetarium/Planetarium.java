c49cd2a7d37708083f2ed3aab2987ef1301c308b
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-03-27 15:18:54 +0200

    Setup java project
    Created basic structure of the java project

---
+++ planetarium/Planetarium.java
@@ -0,0 +1,7 @@
+package planetarium;
+
+public class Planetarium {
+    public static void main(String[] args) {
+        System.out.println("Planetarium");
+    }
+}
---
+++ planetarium/SolarSystem.java
@@ -0,0 +1,4 @@
+package planetarium;
+
+public class SolarSystem {
+}
