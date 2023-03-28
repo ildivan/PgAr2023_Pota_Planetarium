7868ddae8da6563ed826085bbc8f1eb57c0df319
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-03-28 14:41:03 +0200

    Adjusted SolarSystem constructor

--- planetarium/solarsystem/SolarSystem.java
+++ planetarium/solarsystem/SolarSystem.java
@@ -7,12 +7,13 @@ import java.util.List;
 public class SolarSystem {
     private final Star star;
 
+
     public SolarSystem(Position starPosition, long starMass){
         star = new Star(starPosition,starMass);
     }
 
     public SolarSystem(long x,long y, long starMass){
-        star = new Star(new Position(x,y),starMass);
+        star = new Star(x,y,starMass);
     }
 
     public Star getStar(){
