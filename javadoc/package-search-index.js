6a631f45bb6e192b8808255d0d2a92412c11506e
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-04-11 20:17:48 +0200

    Generated javadoc and created UML
    Generated javadoc but only for the solarsystem package and created uml for the project.

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"planetarium"},{"l":"planetarium.solarsystem"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"planetarium.solarsystem"}];updateSearchResults();
